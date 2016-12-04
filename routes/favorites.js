0377d2242e1b040da8f5fa793d56fd7ed9d0dfe8
Cassie Peterson <cas.j.peters@gmail.com>
2016-12-04 14:36:49 -0700

    6 of 10 tests pass in favorites routing, POST and DELETE have not been created

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,62 @@
 'use strict';
 
 const express = require('express');
-
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const jwt = require('jsonwebtoken');
+const boom = require('boom');
 
-// YOUR CODE HERE
+const {
+    camelizeKeys,
+    decamelizeKeys
+} = require('humps');
 
+
+router.get('/favorites', function(req, res, next) {
+    jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return next(boom.create(401, 'Unauthorized'))
+        } else {
+            console.log(decoded);
+            knex('favorites')
+                .join('books', 'favorites.book_id', '=', 'books.id')
+                .where('favorites.user_id', decoded.id)
+                .then((books) => {
+                    res.send(camelizeKeys(books))
+                })
+        }
+    })
+
+})
+
+router.get('/favorites/check', function(req, res, next) {
+    const bookId = req.query.bookId;
+    if (isNaN(bookId)) {
+      return next(boom.create(401), 'Unauthorized');
+    }
+
+    jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return next(boom.create(401, 'Unauthorized'))
+      }
+        knex('favorites')
+            .join('books', 'favorites.book_id', '=', 'books.id')
+            .where('favorites.user_id', decoded.id)
+            .andWhere('books.id', bookId)
+            .then((returnedBook) => {
+              if (returnedBook.length) {
+                return res.send(true)
+              }else {
+                res.send(false)
+              }
+            })
+            .catch((err) => {
+                return next(boom.create(401, 'Unauthorized'));
+            })
+        .catch((err) =>{
+          return next(err);
+        })
+    })
+})
 module.exports = router;
--- routes/token.js
+++ routes/token.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt-as-promised');
 const knex = require('../knex');
 const boom = require('boom');
+const jwt = require('jsonwebtoken');
 
 const {
     camelizeKeys,
@@ -14,36 +15,60 @@ const {
 
 
 // YOUR CODE HERE
-router.get('/token', function (req,res,next) {
-
+router.get('/token', function(req, res, next) {
+    jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+        if (decoded || err === null) {
+            res.send(true)
+        } else {
+            res.send(false)
+        }
+    })
 })
 
-router.post('/token', function (req, res, next) {
-  const {email, password} = req.body;
-  return knex('users')
-  .where('email', email)
-  .first()
-  .then((result) => {
-    var thisUser = result;
-    // console.log(result, 'This is the result');
-    if (!result) {
-      return next(boom.create(400, 'Bad email or password'));
-    } else {
-      bcrypt.compare(password, result.hashed_password)
-      .then((resultCompare) => {
-        
-        res.send({id: thisUser.id,
-          email: thisUser.email
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        next(boom.create(400, 'Bad email or password'));
-      });
-    }
+router.post('/token', function(req, res, next) {
+    const {
+        email,
+        password
+    } = req.body;
+    return knex('users')
+        .where('email', email)
+        .first()
+        .then((result) => {
+            var thisUser = result;
+            // console.log(result, 'This is the result');
+            if (!thisUser) {
+                return next(boom.create(400, 'Bad email or password'));
+            } else {
+                bcrypt.compare(password, thisUser.hashed_password)
+                    .then((resultCompare) => {
+                        var token = jwt.sign({
+                            id: thisUser.id,
+                            email: email
+                            // password: thisUser.hashed_password
+                        }, process.env.JWT_SECRET);
 
-    })
-    res.send('email exists')
-  })
+                        res.cookie('token', token, {
+                            httpOnly: true
+                        });
+                        res.send(camelizeKeys({
+                            id: thisUser.id,
+                            email: thisUser.email,
+                            first_name: thisUser.first_name,
+                            last_name: thisUser.last_name,
+                        }));
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        return next(boom.create(400, 'Bad email or password'));
+                    });
+            }
+
+        })
+})
+
+router.delete('/token', function(req, res, next) {
+    res.clearCookie('token');
+    res.send(true);
+})
 
 module.exports = router;
